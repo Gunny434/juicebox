c57991fa5adf155a346cbd5ce599bda081c46d16
Tyler Gunlock <ty_gunlock@yahoo.com>
2022-12-29 16:49:07 -0500

    Part 2.2 complete

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
-
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
 server.use(express.json());
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 const { client } = require('./db');
 client.connect();
